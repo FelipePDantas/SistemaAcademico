398d44f2c6578d05937b957ff28e48b856fcdbec
felipe <felipejiti@hotmail.com>
2024-06-26 14:32:37 -0300

    core da Api criado

---
+++ src/main/kotlin/com/SistemaAcademico/ApiSistemaAcademico/ApiSistemaAcademicoApplication.kt
@@ -0,0 +1,11 @@
+package com.SistemaAcademico.ApiSistemaAcademico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiSistemaAcademicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiSistemaAcademicoApplication>(*args)
+}
---
+++ src/test/kotlin/com/SistemaAcademico/ApiSistemaAcademico/ApiSistemaAcademicoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.SistemaAcademico.ApiSistemaAcademico
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiSistemaAcademicoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
